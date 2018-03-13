0b00e6a96da5c0a2005f88472f4f826e65e7ac0b
Admin <admin@martcoin.io>
2018-03-13 16:08:24 +0700

    init smart contract base on erc20 for martcoin

---
+++ contracts/Owned.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.18;
+
+contract owned {
+    address public owner;
+
+    function owned() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner public {
+        owner = newOwner;
+    }
+}
+
+interface tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
